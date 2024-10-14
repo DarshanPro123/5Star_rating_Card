2638210562bd63dca3b66ce6018346c193b961ce
Darshanpro123 <darshanpro187@gmail.com>
2024-10-14 13:30:31 +0530

    Edit all style and add some another card of movie

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,57 @@ import Rating_card from "./components/Rating_Card.jsx";
 
 function App() {
   const maxRating = 5;
-  const [tempRate, setTempRate] = useState(0);
 
-  const [rating, setRate] = useState(0);
+  // Define an array of movies with their respective images and names
+  const movies = [
+    {
+      image:
+        "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/pushpa--the-rise-et00129538-08-12-2021-01-21-46.jpg",
+      name: "PUSHPA",
+    },
+    {
+      image:
+        "https://i.pinimg.com/236x/8f/25/db/8f25db5d5f9a6d6fd3cdc7b993e8cb06.jpg",
+      name: "DARBAR",
+    },
+    {
+      image:
+        "https://e1.pxfuel.com/desktop-wallpaper/858/754/desktop-wallpaper-baahubali-pushpa-rrr-the-south-rises-as-bollywood-appears-fake-out-of-touch-bollywood-movie-poster-2022-thumbnail.jpg",
+      name: "RRR",
+    },
+    {
+      image:
+        "https://rukminim2.flixcart.com/image/850/1000/xif0q/poster/i/x/r/small-pk-aamir-khan-bollywood-hindi-movie-poster-skd4-dpost-original-imagpmz8fcczaupz.jpeg?q=90&crop=false",
+      name: "pk",
+    },
+  ];
+
   return (
     <>
-      <Rating_card>
-        {" "}
-        <Star_rating
-          maxRating={maxRating}
-          tempRate={tempRate}
-          setTempRate={setTempRate}
-          rating={rating}
-          setRate={setRate}
-          messages={[
-            "Not good 😕", // 1 star
-            "Could be better 🙂", // 2 stars
-            "Average 😃", // 3 stars
-            "Good 😇", // 4 stars
-            "Excellent 🤩", // 5 stars
-          ]}
-        />
-      </Rating_card>
+      {movies.map((movie, index) => {
+        // Create local state for each movie's rating
+        const [tempRate, setTempRate] = useState(0);
+        const [rating, setRate] = useState(0);
+
+        return (
+          <Rating_card key={index} image={movie.image} name={movie.name}>
+            <Star_rating
+              maxRating={maxRating}
+              tempRate={tempRate}
+              setTempRate={setTempRate}
+              rating={rating}
+              setRate={setRate}
+              messages={[
+                "Not good 😕", // 1 star
+                "Could be better 🙂", // 2 stars
+                "Average 😃", // 3 stars
+                "Good 😇", // 4 stars
+                "Excellent 🤩", // 5 stars
+              ]}
+            />
+          </Rating_card>
+        );
+      })}
     </>
   );
 }
--- src/components/Rating_Card.jsx
+++ src/components/Rating_Card.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 
-const Rating_Card = ({ children }) => {
+const Rating_Card = ({ children, image, name }) => {
   return (
     <div className="card">
-      <img
-        src={
-          "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        }
-        alt="Hotel"
-      />
+      <img src={image} alt="Hotel" />
 
       <div className="content">
         <div>
           {" "}
-          <h1>Mergistre</h1>
+          <h1>{name}</h1>
           <span>{children}</span>
         </div>
         <p>
           Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur,
           adipisicing elit. Veritatis deserunt, eum aspernatur enim nesciunt
-          doloribus ex odio similique, officiis ducimus quidem vel placeat.
+          doloribus ex odio similique, Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Eos eveniet perferendis autem sit, iure quos itaque
+          ut sunt voluptas expedita pariatur repellat exercitationem animi
+          incidunt, doloremque distinctio magnam? Laborum, quaerat. officiis
+          ducimus quidem vel placeat.
         </p>
         <button>Book Now</button>
       </div>
